477efab083b40a649190217668ae486ce172f065
JamieKalloe <Jamie@Kalloe.net>
2016-01-25 16:17:58 +0100

    test code home

--- flask_server.py
+++ flask_server.py
@@ -268,14 +268,15 @@ def home():
 	loop = True
 	#while loop:
 	while loop:
-		for pin in range(0, 4):
+		'''for pin in range(0, 4):
 			xpin = StepPins[pin]
 			if Seq[StepCounter][pin]!=0:
 				#print " Step %i Enable %i" %(StepCounter,xpin)
 				GPIO.output(xpin, True)
 			else:
 				GPIO.output(xpin, False)
-			StepCounter += 1
+			StepCounter += 1'''
+			rotateMotor(steps=9000,direction="UP")	
 			curr_state = GPIO.input(buttonPin)
 			if (curr_state != prev_state):
 				if (curr_state == 0):
