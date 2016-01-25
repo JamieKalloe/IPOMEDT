cf88d3623469de5b46b52dfd438cf090df627303
JamieKalloe <Jamie@Kalloe.net>
2016-01-25 16:10:30 +0100

    deleted reduntant while loop

--- flask_server.py
+++ flask_server.py
@@ -266,32 +266,32 @@ def home():
 	# initialize event
 	event = 1
 	loop = True
+	#while loop:
 	while loop:
-		while loop:
-			for pin in range(0, 4):
-				xpin = StepPins[pin]
-				if Seq[StepCounter][pin]!=0:
-					#print " Step %i Enable %i" %(StepCounter,xpin)
-					GPIO.output(xpin, True)
-				else:
-					GPIO.output(xpin, False)
-				StepCounter += 1
-				curr_state = GPIO.input(buttonPin)
-				if (curr_state != prev_state):
-					if (curr_state == 0):
-						event = "pressed"
-						print event
-						#sys.exit()
-						loop = False
-				prev_state = curr_state
-				# If we reach the end of the sequence
-				# start again
-				if (StepCounter==StepCount):
-					StepCounter = 0
-				if (StepCounter<0):
-					StepCounter = StepCount
-				# Wait before moving on
-				time.sleep(WaitTime)
+		for pin in range(0, 4):
+			xpin = StepPins[pin]
+			if Seq[StepCounter][pin]!=0:
+				#print " Step %i Enable %i" %(StepCounter,xpin)
+				GPIO.output(xpin, True)
+			else:
+				GPIO.output(xpin, False)
+			StepCounter += 1
+			curr_state = GPIO.input(buttonPin)
+			if (curr_state != prev_state):
+				if (curr_state == 0):
+					event = "pressed"
+					print event
+					#sys.exit()
+					loop = False
+			prev_state = curr_state
+			# If we reach the end of the sequence
+			# start again
+			if (StepCounter==StepCount):
+				StepCounter = 0
+			if (StepCounter<0):
+				StepCounter = StepCount
+			# Wait before moving on
+			time.sleep(WaitTime)
 	GPIO.cleanup()
 	return render_template('index.html',)
 
