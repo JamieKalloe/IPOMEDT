936ca4eef362ae11548cc837dcb53d260d903337
JamieKalloe <Jamie@Kalloe.net>
2016-01-27 19:53:08 +0100

    adjust

--- flask_server.py
+++ flask_server.py
@@ -29,7 +29,7 @@ def autoRun():
         #global isRunning
         while isRunning == True:
             #light()
-            homef()
+            # homef()
             #tpid = getPid()
             #if (tpid != 1):
             #    sensor()
