9dca9748fe9792d7a8c386e8a00d2ab52a9b5697
JamieKalloe <Jamie@Kalloe.net>
2016-01-27 17:02:04 +0100

    3w434

--- flask_server.py
+++ flask_server.py
@@ -21,8 +21,7 @@ def autoRun():
     global isRunning
     while True:
         if tpid != 1 and checkSensor() == 1:
-            #isRunning = True
-            auto_on()
+            isRunning = True
         else:
             #auto_off()
             GPIO.cleanup()
